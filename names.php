411e1d0e78e0491095d63cd5c862d8321d647be0
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-04-03 16:28:17 -0400

    add query

---
+++ names.php
@@ -0,0 +1,21 @@
+<?php
+// Pris des notes de cours Rest.pdf
+
+require "/.config.php";
+
+$query = "SELECT NAME FROM brasseries";
+
+// Execute query
+try
+{
+ $res = $pdo->prepare($query);
+ $res->execute();
+}
+catch (PDOException $e)
+{
+ /* If there is a PDO exception, throw a standard exception */
+ throw new Exception('Database query error');
+}
+echo json_encode($res->fetchAll(PDO::FETCH_ASSOC));
+
+?>
