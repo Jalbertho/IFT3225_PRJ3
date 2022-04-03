5f71edf2450b8f13b083482b2405bb970f72c452
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-04-03 17:05:21 -0400

    debug

--- names.php
+++ names.php
@@ -3,25 +3,19 @@
 
 require "./config.php";
 
-echo "Debug: " . $debug;
+$query = 'SELECT NAME FROM brasseries';
 
-// echo "".$pdo;
-
-// $query = 'SELECT NAME FROM brasseries';
-
-// echo $query;
-
-// // Execute query
-// try
-// {
-//  $res = $pdo->prepare($query);
-//  $res->execute();
-// }
-// catch (PDOException $e)
-// {
-//  /* If there is a PDO exception, throw a standard exception */
-//  throw new Exception('Database query error');
-// }
-// echo json_encode($res->fetchAll(PDO::FETCH_ASSOC));
+// Execute query
+try
+{
+ $res = $pdo->prepare($query);
+ $res->execute();
+}
+catch (PDOException $e)
+{
+ /* If there is a PDO exception, throw a standard exception */
+ throw new Exception('Database query error');
+}
+echo json_encode($res->fetchAll(PDO::FETCH_ASSOC));
 
 ?>
