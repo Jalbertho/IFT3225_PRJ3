f4b7abd86c0bb1e9f116ed1121283409807659a5
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-04-05 21:55:25 -0400

    ddebug

--- index.php
+++ index.php
@@ -29,13 +29,11 @@ function run ($p) {
 }
 
 $r->add('/names', array(), function(Route $route) {
-	echo '/names/';
     run("/names.php".$route->pfx);
     die();
 });
 
 $r->add('/names/:pfx', array(), function(Route $route) {
-	echo '/names/pfx';
     run("/names.php?pfx=".$route->pfx);
     die();
 });
