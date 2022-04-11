5a9dce03ee3a6ab97165fc6497e8a730ad074d03
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-04-11 13:27:26 -0400

    debug

--- queries/add.php
+++ queries/add.php
@@ -11,7 +11,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 // include database and object files
 require "./config.php";
 
-echo $_GET["input"]
+echo " ".$_GET["input"];
 
 // update query
 // $query = "INSERT INTO brasseries (
