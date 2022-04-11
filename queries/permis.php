4337d0db854db6004487d2ea1462f89359bc749d
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-04-11 11:17:07 -0400

    debug

--- queries/permis.php
+++ queries/permis.php
@@ -3,12 +3,10 @@
 
 require "./config.php";
 
-echo ":)";
-
 // if (! (isset($_GET['permis'])))
 //   throw new Exception('Query must take an argument representing the permit number.')
 
-$query = "SELECT * FROM brasseries WHERE numPermit='".$_GET['num']."%'";
+$query = "SELECT * FROM brasseries WHERE numPermit='".$_GET['num']."'";
 
 // Execute query
 try
