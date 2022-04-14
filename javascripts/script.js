a256ba5f03da90fa4fcf7ac7f04abece32e20888
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-04-14 19:29:00 -0400

    implement d3

--- javascripts/script.js
+++ javascripts/script.js
@@ -21,11 +21,103 @@ var getBrasseries = function() {
   return result;
 }
 
-var drawCircle = function() {
-  var width = 500;
-  var height = 500;
+var drawPlot = function() {
+  var width = 800;
+  var height = 600;
 
-  var svg = d3.select("body").append("#main").append("svg").attr("width", width).attr("height", height);
+  var margin = {
+    top: 20,
+    right: 30,
+    bottom: 30,
+    left: 40
+  };
+
+  var data = getBrasseries();
+  data.splice(50); // TODO.. to many data otherwise ?
+
+  console.log(data);
+
+  var x = d3.scaleLinear()
+              .domain(d3.extent(data, d => d.longitude)).nice()
+              .range([margin.left, width - margin.right]);
+
+  var y = d3.scaleLinear()
+              .domain(d3.extent(data, d => d.latitude)).nice()
+              .range([height - margin.bottom, margin.top]);
+
+  var xAxis = g => g
+                .attr("transform", `translate(0,${height - margin.bottom})`)
+                .call(d3.axisBottom(x))
+                .call(g => g.select(".domain").remove())
+                .call(g => g.append("text")
+                            .attr("x", width - margin.right)
+                            .attr("y", -4)
+                            .attr("fill", "#000")
+                            .attr("font-weight", "bold")
+                            .attr("text-anchor", "end")
+                            .text('Longitude'));
+
+  var yAxis = g => g
+                .attr("transform", `translate(${margin.left},0)`)
+                .call(d3.axisLeft(y))
+                .call(g => g.select(".domain").remove())
+                .call(g => g.select(".tick:last-of-type text").clone()
+                            .attr("x", 4)
+                            .attr("text-anchor", "start")
+                            .attr("font-weight", "bold")
+                            .text('Latitude'));
+
+  var grid = g => g
+                .attr("stroke", "currentColor")
+                .attr("stroke-opacity", 0.1)
+                .call(g => g.append("g")
+                            .selectAll("line")
+                            .data(x.ticks())
+                            .join("line")
+                            .attr("x1", d => 0.5 + x(d))
+                            .attr("x2", d => 0.5 + x(d))
+                            .attr("y1", margin.top)
+                            .attr("y2", height - margin.bottom))
+                            .call(g => g.append("g")
+                                        .selectAll("line")
+                                        .data(y.ticks())
+                                        .join("line")
+                                        .attr("y1", d => 0.5 + y(d))
+                                        .attr("y2", d => 0.5 + y(d))
+                                        .attr("x1", margin.left)
+                                        .attr("x2", width - margin.right));
+
+   var svg = d3.select("#viz")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height);
+
+   svg.append("g").call(xAxis);
+   svg.append("g").call(yAxis);
+   svg.append("g").call(grid);
+
+   svg.append("g")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("fill", "none")
+        .selectAll("circle")
+        .data(data)
+        .join("circle")
+        .attr("cx", d => x(d.x))
+        .attr("cy", d => y(d.y))
+        .attr("r", 3);
+
+
+   svg.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .selectAll("text")
+        .data(data)
+        .join("text")
+        .attr("dy", "0.35em")
+        .attr("x", d => x(d.x) + 7)
+        .attr("y", d => y(d.y))
+        .text(d => d.name);
 }
 
 var app = $.sammy('#main', function() {
@@ -187,9 +279,9 @@ var app = $.sammy('#main', function() {
     context.app.swap('');
 
     if (sessionStorage.getItem('priviledge')) {
-      var result = getBrasseries();
-
       alert("Yaaas");
+      drawPlot();
+
 
     }else{
       alert("no");
