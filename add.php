f901421da6714f99e68f235b237317428eb93f17
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-04-17 10:43:28 -0400

    debug

--- add.php
+++ add.php
@@ -15,11 +15,11 @@ echo "REQUEST :: ADD <br>";
 echo "".$_GET['input'];
 echo "<br>";
 $input = json_decode(file_get_contents("php://input"));
-$obj = json_decode($_GET['input']);
-echo "DATA : ".$input. "<br>";
-echo "OBJ : ".$obj."<br>";
+// $obj = json_decode($_GET['input']);
+// echo "DATA : ".$input. "<br>";
+// echo "OBJ : ".$obj."<br>";
 // echo "".$obj["name"];
-echo "".$obj->name;
+// echo "".$obj->name;
 // echo "".$input["name"];
 echo "".$input->name;
 
