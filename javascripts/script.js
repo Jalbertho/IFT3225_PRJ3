448a9c26d1d0035fbb5ad08d2a03bd9906011e22
Kathrine Jalbert <kathrine.jalbert@hotmail.com>
2022-04-18 13:15:52 -0400

    oops

--- javascripts/script.js
+++ javascripts/script.js
@@ -242,7 +242,7 @@ var app = $.sammy('#main', function() {
     console.log("https://www-ens.iro.umontreal.ca/~jalbertk/fyWdSJ8v/PRJ3/App/add/"+this.params);
 
     $.ajax({
-      url: "https://www-ens.iro.umontreal.ca/~jalbertk/fyWdSJ8v/PRJ3/App/add/"+this.params;
+      url: "https://www-ens.iro.umontreal.ca/~jalbertk/fyWdSJ8v/PRJ3/App/add/"+this.params,
       type : "POST",
       dataType : 'json',
       success : function(response) {
